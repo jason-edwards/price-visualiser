e879172957496156b8e410f601024416a2ba3d8e
Jason Edwards <jedwards@live.com.au>
2015-06-09 17:58:22 +1000

    Added webservices.py

---
+++ webservice.py
@@ -0,0 +1,41 @@
+__author__ = 'jason'
+
+from flask import Flask, request, redirect
+from daemon import Daemon
+import sys
+
+
+WEB_PORT = 5000 if platform.system() == "Darwin" else 80
+app = Flask(__name__)
+#app.debug = True
+
+
+class MyDaemon(Daemon):
+    def run(self):
+        app.run(host='0.0.0.0', port=WEB_PORT)
+
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+
+    if argc == 2:
+        daemon = MyDaemon('/tmp/daemon-example.pid')
+        if 'start' == sys.argv[1]:
+            print "Starting as daemon."
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            print "Restarting as daemon."
+            daemon.restart()
+        elif 'history' == sys.argv[1]:
+            print "usage: %s history code" % sys.argv[0]
+            sys.exit(2)
+        else:
+            print "usage: %s start|stop|restart\n" % sys.argv[0]
+            sys.exit(2)
+    else:
+        print "Not running as daemon!"
+        app.run(host='0.0.0.0', port=WEB_PORT)
+
+print "Exiting"
