dfa61ec3d61c8cafee13bddd2b8810f480f60cad
Shalvin Deo <shalvin.deo@live.com>
2016-02-15 20:03:18 +1000

    Added base abstract classes for token sources and price sources.

---
+++ price_source.py
@@ -0,0 +1,17 @@
+﻿from enum import Enum
+
+
+class PriceSourceType(Enum):
+    scrape      = 1
+    api         = 2
+
+
+class PriceSource:
+
+    def __init__(self, stock: str, sourcetype: SourceType):
+        self.stock = stock
+        self.sourcetype = sourcetype
+
+    def get_price() -> double:
+        # Override in subclass
+        pass
---
+++ token_source.py
@@ -0,0 +1,16 @@
+﻿from enum import Enum
+
+
+class TokenSourceType(Enum):
+    rss         = 1
+    twitter     = 2
+
+
+class TokenSource:
+
+    def __init__(self, official:bool):
+        self.official = official
+
+    def get_token_blob() -> str:
+        # Override in subclass
+        pass
