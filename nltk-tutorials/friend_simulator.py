04056517365a3f2063737051576fa5bc8104ef26
Shalvin Deo <shalvin.deo@live.com>
2016-02-28 13:30:31 +1000

    Added a 'friend simulator' for learning about NLTK. Download your facebook data in Account Settings, at the bottom of General. Move the html/message.htm file into the nltk-tutorials folder Then run like this: python friend_simulator.py -l 100 -r 5 "Alex Loch" -s "kappa" where -l is the output length (number of words), -r is the randomness when choosing the most_common words and -s is the seed word. If running for the first time, specify -g which will generate the 'mini corpus'.

---
+++ nltk-tutorials/friend_simulator.py
@@ -0,0 +1,89 @@
+﻿from bs4 import BeautifulSoup
+import bs4
+from datetime import datetime
+from whatever import shit_talk
+import argparse
+
+# Create a text file containing raw message texts. Newlines are created for each message but each message may have their own newlines.
+def friend_threads(friendname, fbmessagesrawhtml, afterdate=None):
+    print("Making soup.")
+    soup = BeautifulSoup(fbmessagesrawhtml, "lxml")
+
+    print("Searching for relevant threads.")
+    filteredthreads = []
+    threadtags = soup.find_all("div", {"class":"thread"})
+    for t in threadtags:
+        threadparticipants = t.contents[0]
+        if friendname in threadparticipants:
+            filteredthreads.append(t)
+    print("Found %d threads" % len(filteredthreads))
+
+    print("Finding messages.")
+    earliestmessage = datetime.now()
+    earliestmessagecontents = ''
+    matchingmessages = []
+    updateearliestmessage = False
+    for t in filteredthreads:
+        print("Messages: %d" % len(matchingmessages))
+        messageheaders = t.find_all("div", {"class":"message"})
+
+        for h in messageheaders:
+            usertag = h.find("span", {"class":"user"})
+            if usertag.contents[0] and usertag.contents[0] != friendname:
+                continue
+            
+            datetag = h.find("span", {"class":"meta"})
+            messagedate = datetime.strptime(datetag.contents[0], "%A, %B %d, %Y at %I:%M%p UTC+10")
+            if messagedate < earliestmessage:
+                earliestmessage = messagedate
+                updateearliestmessage = True
+            if afterdate and afterdate > messagedate:
+                continue
+
+            message = h.find_next('p')
+            if message and message.name == 'p' and len(message.contents) > 0:
+                matchingmessages.append(message.contents[0])
+                if updateearliestmessage:
+                    earliestmessagecontents = message.contents[0]
+                    updateearliestmessage = False
+
+    print("Found %d messages in total." % len(matchingmessages))
+    print("Earliest message sent on %s: '%s'" % (str(earliestmessage), str(earliestmessagecontents)))
+    
+    if len(matchingmessages) > 0:
+        print("Writing messages to file.")
+        with open("Shit %s Says.txt" % friendname, 'w', encoding='utf8') as f:
+            for s in matchingmessages:
+                f.write(str(s))
+                f.write('\n')
+    else:
+        print("No messages found for %s" % friendname)
+
+
+# Run stuff
+argparser = argparse.ArgumentParser()
+argparser.add_argument("friend", type=str, help="Person's first name and last name with the first letter of each name capitalised.")
+argparser.add_argument("-s", "--seed", type=str, help="Seed word.")
+argparser.add_argument("-g", "--gen", action="store_true", help="Generate corpus data.")
+argparser.add_argument("-m", "--mute", action="store_true", help="Don't shit talk.")
+argparser.add_argument("-r", "--randomness", type=int, help="How random each word selection should be. 1 means use top word.")
+argparser.add_argument("-l", "--length", type=int, help="Length of shit talking.")
+args = argparser.parse_args()
+
+friendname = args.friend
+seedword = args.seed
+filepath = "messages.htm"
+
+fbmessagesrawhtml = ""
+with open(filepath, encoding='utf8') as f:
+    fbmessagesrawhtml = f.read()
+
+startofthisyear = datetime(2016, 1, 1, 0)
+
+randomness = args.randomness if args.randomness else 5
+outputlength = args.length if args.length else 50
+
+if args.gen:
+    friend_threads(friendname, fbmessagesrawhtml)
+if args.seed:
+    shit_talk(friendname, seedword, randomness, outputlength)
---
+++ nltk-tutorials/whatever.py
@@ -0,0 +1,50 @@
+﻿import nltk
+from nltk.corpus import webtext
+import random
+import matplotlib.pyplot as plt
+import numpy
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#c = 500000
+
+#random_string = ''
+#for i in range(c):
+#    random_string += random.choice(alphabet + ' ')
+
+##fdist = nltk.FreqDist(random_string.split())
+#fdist = nltk.FreqDist(book.text1)
+#sortedvalues = sorted(fdist.values(), reverse=True)
+#lnrange = [numpy.log10(x) for x in range(1, len(sortedvalues)+1)]
+
+#print(fdist.most_common(50))
+
+#plt.plot(lnrange, sortedvalues)
+#plt.show()
+
+
+def generate_model(cfdist, word, n, num):
+    for i in range(num):
+        print(word, end=' ')
+        #word = cfdist[word].max()
+        ncommonwords = cfdist[word].most_common(n)
+        if len(ncommonwords) == 0:
+            print("<Ran out of words!>")
+            break
+        word = random.choice(ncommonwords)[0]
+
+#text = webtext.words()
+def shit_talk(friendname, seedword, n=3, num=50):
+    with open("Shit %s Says.txt" % friendname, encoding='utf8') as f:
+        rawtext = f.read()
+        text = nltk.tokenize.casual_tokenize(rawtext)
+    lowercasetext = [i.lower() for i in text]
+
+    filteredtokens = []
+    for w in lowercasetext:
+        if w[0] in alphabet:
+            filteredtokens.append(w)
+
+    bigrams = nltk.bigrams(filteredtokens)
+    cfd = nltk.ConditionalFreqDist(bigrams)
+
+    generate_model(cfd, seedword, n, num)
